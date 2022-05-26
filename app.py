53f12a5e3457539b6ca7cf776f9f6d261c56174e
GlebMaramzin <g.maramzin@yandex.ru>
2022-05-26 20:41:15 +0300

    new commit

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import sys
-
-from flask import Flask, render_template, request, flash, redirect, url_for
+# cn5jQki0
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user
+from flask_login import LoginManager, UserMixin, current_user, login_required, login_user
 from forms import LoginForm, CreateProjectForm, CreateTaskForm
+from flask_socketio import SocketIO, send
 from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash
 import os
 from datetime import datetime
 
 
 UPLOAD_FOLDER = 'userfiles'
 ALLOWED_EXTENSIONS = {'txt', 'word', 'png'}
-
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
 app.debug = True
 app.config['SECRET_KEY'] = 'i3o482309g8s0d903kmmxcnvm2109349320vddfew3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,21 +27,24 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Необходимо авторизироваться!'
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return db.session.query(User).get(user_id)
+@socketio.on('message')
+def handle_message(data):
+    send(data, broadcast=True)
+
+
+def allowed_files(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 class Project(db.Model):
     __tablename__ = 'Project'
 
-    ProjectId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    Title = db.Column(db.String(100), nullable=False)
-    Desc = db.Column(db.Text, nullable=False)
+    ProjectTitle = db.Column(db.String(100), primary_key=True)
+    Desc = db.Column(db.Text(), nullable=False)
     Deadline = db.Column(db.Date(), nullable=False)
-    Leader = db.Column(db.String(250), nullable=False)
+    Leader = db.Column(db.String(150), nullable=False)
     DateCreate = db.Column(db.DateTime(), default=datetime.utcnow(), nullable=False)
-    IsEnd = db.Column(db.Boolean, default=False, nullable=False)
+    IsCompleted = db.Column(db.Boolean, default=False, nullable=False)
 
 
 class Role(db.Model):
@@ -50,21 +54,18 @@ class Role(db.Model):
     Title = db.Column(db.String(30), nullable=False)
 
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'User'
+class UserIcon(db.Model):
+    __tablename__ = 'UserIcon'
 
-    UserId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    Login = db.Column(db.String(12), nullable=False)
-    Password = db.Column(db.Text(), nullable=False)
-    FIO = db.Column(db.String(250), nullable=False)
-    RoleId = db.Column(db.Integer(), db.ForeignKey('Role.RoleId'))
-    DateCreate = db.Column(db.DateTime(), default=datetime.utcnow())
-    ProjectId = db.Column(db.Integer(), db.ForeignKey('Project.ProjectId'))
+    UserIconId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    Path = db.Column(db.String(200), nullable=False)
 
-    Role = db.relationship('Role', backref='User', uselist=False)
 
-    def get_id(self):
-        return self.UserId
+class FileType(db.Model):
+    __tablename__ = 'FileType'
+
+    FileTypeId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    Title = db.Column(db.String(8), nullable=False)
 
 
 class TaskStatus(db.Model):
@@ -74,56 +75,85 @@ class TaskStatus(db.Model):
     Title = db.Column(db.String(30), nullable=False)
 
 
-class Task(db.Model):
-    __tablename__ = 'Task'
+class User(db.Model, UserMixin):
+    __tablename__ = 'User'
 
-    TaskId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    ProjectId = db.Column(db.Integer(), db.ForeignKey('Project.ProjectId'))
-    Title = db.Column(db.String(50), nullable=False)
-    Desc = db.Column(db.Text, nullable=False)
-    Deadline = db.Column(db.Date())
-    IsPriority = db.Column(db.Boolean(), nullable=False)
-    TaskStatusId = db.Column(db.Integer(), db.ForeignKey('TaskStatus.TaskStatusId'))
+    Login = db.Column(db.String(20), primary_key=True)
+    Password = db.Column(db.Text(), nullable=False)
+    RoleId = db.Column(db.Integer(), db.ForeignKey('Role.RoleId'))
+    UserIconId = db.Column(db.Integer(), db.ForeignKey('UserIcon.UserIconId'))
+    FIO = db.Column(db.String(150), nullable=False)
     DateCreate = db.Column(db.DateTime(), default=datetime.utcnow())
+    ProjectTitle = db.Column(db.String(100), db.ForeignKey('Project.ProjectTitle'))
 
+    Role = db.relationship('Role', backref='User', uselist=False)
+    Icon = db.relationship('UserIcon', backref='User', uselist=False)
+    Project = db.relationship('Project', backref='User', uselist=False)
 
-class Repository(db.Model):
-    __tablename__ = 'Repository'
-
-    RepositoryId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    ProjectId = db.Column(db.Integer(), db.ForeignKey('Project.ProjectId'))
-    DateCreate = db.Column(db.DateTime(), default=datetime.utcnow())
-
-
-class RepositoryFile(db.Model):
-    __tablename__ = 'RepositoryFile'
-
-    RepositoryFileId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    Path = db.Column(db.String(200), nullable=False)
-    RepositoryId = db.Column(db.Integer(), db.ForeignKey('Repository.RepositoryId'))
-    DateLoad = db.Column(db.DateTime(), default=datetime.utcnow())
+    def get_id(self):
+        return self.Login
 
 
 class ChatMessage(db.Model):
     __tablename__ = 'ChatMessage'
 
-    ChatMessageId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    UserId = db.Column(db.Integer(), db.ForeignKey('User.UserId'))
+    DateTimeSend = db.Column(db.DateTime(), primary_key=True, default=datetime.utcnow())
+    Login = db.Column(db.String(20), db.ForeignKey('User.Login'), primary_key=True)
     Text = db.Column(db.Text(), nullable=False)
-    DateSend = db.Column(db.DateTime(), default=datetime.utcnow())
-    ProjectId = db.Column(db.Integer(), db.ForeignKey('Project.ProjectId'))
 
 
-def allowed_files(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+class Task(db.Model):
+    __tablename__ = 'Task'
+
+    TaskId = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    Login = db.Column(db.String(20), db.ForeignKey('User.Login'))
+    Title = db.Column(db.String(50), nullable=False)
+    Desc = db.Column(db.Text, nullable=False)
+    Deadline = db.Column(db.Date())
+    IsPriority = db.Column(db.Boolean(), nullable=False)
+    TaskStatusId = db.Column(db.Integer(), db.ForeignKey('TaskStatus.TaskStatusId'))
+    DateCreate = db.Column(db.Date(), default=datetime.utcnow())
+    DateComplete = db.Column(db.Date())
+
+
+class TaskFile(db.Model):
+    __tablename__ = 'TaskFile'
+
+    Filename = db.Column(db.String(50), primary_key=True)
+    FileTypeId = db.Column(db.Integer, db.ForeignKey('FileType.FileTypeId'))
+    TaskId = db.Column(db.Integer, db.ForeignKey('Task.TaskId'))
+    Path = db.Column(db.String(200), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.query(User).get(user_id)
 
 
 @app.route('/tasklist')
 @login_required
 def tasklist():
-    curr_user = db.session.query(User).filter(User.UserId == current_user.get_id()).first()
+    curr_user = db.session.query(User).filter(User.Login == current_user.get_id()).first()
+    tasks = db.session.query(Task).filter(Task.Login == curr_user.Login).all()
 
-    return render_template('tasklist.html', user_fio=curr_user.FIO, user_role=curr_user.Role.Title)
+    return render_template('tasklist.html',
+                           user_fio=curr_user.FIO,
+                           user_role=curr_user.Role.Title,
+                           user_img=curr_user.Icon.Path,
+                           tasks=tasks)
+
+
+@app.route('/tasklist/<int:task_id>')
+@login_required
+def show_task(task_id):
+    curr_user = db.session.query(User).filter(User.Login == current_user.get_id()).first()
+    curr_task = db.session.query(Task).filter(Task.TaskId == task_id).first()
+
+    return render_template('task.html',
+                           user_fio=curr_user.FIO,
+                           user_role=curr_user.Role.Title,
+                           user_img=curr_user.Icon.Path,
+                           curr_task=curr_task)
 
 
 @app.route('/tasklist', methods=["POST"])
@@ -137,35 +167,84 @@ def upload_files():
         return redirect(url_for('create_project'))
 
 
-@app.route('/project')
+@app.route('/project', methods=["POST", "GET"])
 @login_required
 def project():
-    return render_template('project.html')
+    curr_user = db.session.query(User).filter(User.Login == current_user.get_id()).first()
+
+    if request.method == 'POST':
+        return redirect(url_for('create_project'))
+
+    return render_template('project.html',
+                           user_fio=curr_user.FIO,
+                           user_role=curr_user.Role.Title,
+                           user_img=curr_user.Icon.Path,
+                           project=curr_user.Project)
 
 
 @app.route('/create_project', methods=["POST", "GET"])
 @login_required
 def create_project():
+    curr_user = db.session.query(User).filter(User.Login == current_user.get_id()).first()
     create_project_form = CreateProjectForm()
-    userlist = db.session.query(User).all()
+    userlist = db.session.query(User).filter(User.Login != current_user.get_id()).all()
     if create_project_form.validate_on_submit():
-        developers_id = request.form.getlist('users')
-        print(create_project_form.title.data, file=sys.stderr)
-        print(developers_id, file=sys.stderr)
-        d = []
-        #for id in developers_id:
-            #d.append(db.session.query(User).filter().first())
+        new_project = Project(
+            ProjectTitle=create_project_form.title.data,
+            Desc=create_project_form.desc.data,
+            Deadline=create_project_form.deadline.data,
+            Leader=db.session.query(User).filter(User.Login == current_user.get_id()).first().FIO
+        )
+
+        db.session.add(new_project)
+        db.session.query(User).filter(User.Login == current_user.get_id()).update(
+            {"ProjectTitle": create_project_form.title.data})
+        db.session.commit()
+
+        user_logins = request.form.getlist('users')
+        for user_login in user_logins:
+            db.session.query(User).filter(User.Login == user_login).update(
+                {"ProjectTitle": create_project_form.title.data})
+            db.session.commit()
+
         return redirect(url_for('create_task'))
 
-    return render_template('create_project.html', form=create_project_form, userlist=userlist)
+    return render_template('create_project.html',
+                           user_fio=curr_user.FIO,
+                           user_role=curr_user.Role.Title,
+                           user_img=curr_user.Icon.Path,
+                           form=create_project_form,
+                           userlist=userlist)
 
 
-@app.route('/create_task')
+@app.route('/create_task', methods=["POST", "GET"])
 @login_required
-def create_task(developerlist):
+def create_task():
     create_task_form = CreateTaskForm()
+    curr_user = db.session.query(User).filter(User.Login == current_user.get_id()).first()
 
-    return render_template('create_task.html', form=create_task_form)
+    userlist = db.session.query(User).filter(User.Project != None).all()
+
+    if create_task_form.validate_on_submit():
+        new_task = Task(
+            Login=request.form.getlist('users')[0],
+            Title=create_task_form.title.data,
+            Desc=create_task_form.desc.data,
+            Deadline=create_task_form.deadline.data,
+            IsPriority=create_task_form.isPriority.data,
+            TaskStatusId=2
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for("create_task"))
+
+    return render_template('create_task.html',
+                           user_fio=curr_user.FIO,
+                           user_role=curr_user.Role.Title,
+                           user_img=curr_user.Icon.Path,
+                           form=create_task_form,
+                           userlist=userlist)
 
 
 @app.route('/repository')
@@ -174,25 +253,30 @@ def repository():
     return render_template('repository.html')
 
 
-@app.route('/chat')
+@app.route('/teamchat')
 @login_required
-def chat():
-    return render_template('chat.html')
+def teamchat():
+    curr_user = db.session.query(User).filter(User.Login == current_user.get_id()).first()
+
+    return render_template('teamchat.html',
+                           user_fio=curr_user.FIO,
+                           user_role=curr_user.Role.Title,
+                           user_img=curr_user.Icon.Path,
+                           project=curr_user.Project)
 
 
 @app.route('/', methods=["POST", "GET"])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('tasklist'))
+    # if current_user.is_authenticated:
+    #    return redirect(url_for('tasklist'))
 
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = db.session.query(User).filter(User.Login == login_form.login.data).first()
-        if user and user.Password == login_form.password.data:
+        if user and check_password_hash(user.Password, login_form.password.data):
             login_user(user)
             return redirect(url_for('tasklist'))
 
-        flash('Poshel hanui')
     return render_template('login.html', form=login_form)
 
 
--- forms.py
+++ forms.py
@@ -1,25 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, DateField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    login = StringField("Логин: ", validators=[Length(min=1, max=12)])
+    login = StringField("Логин: ", validators=[Length(min=1, max=40)])
     password = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=1, max=50)])
     submit = SubmitField("Войти")
 
 
 class CreateProjectForm(FlaskForm):
     title = StringField("Название проекта: ", validators=[Length(min=1, max=100)])
-    desc = StringField("Описание проекта: ")
+    desc = TextAreaField("Описание проекта: ")
     deadline = DateField("Дата окончания проекта: ")
     submit = SubmitField("Создать проект")
 
 
 class CreateTaskForm(FlaskForm):
-    title = StringField("Название задачи: ")
-    desc = StringField("Описание задачи: ")
+    title = StringField("Название задачи: ", validators=[Length(min=1, max=100)])
+    desc = TextAreaField("Описание задачи: ")
     deadline = DateField("Дата окончания задачи: ")
-    isPriority = BooleanField("Приоритетная задача: ")
+    isPriority = BooleanField("Приоритетная задача")
     submit = SubmitField("Создать задачу")
 
